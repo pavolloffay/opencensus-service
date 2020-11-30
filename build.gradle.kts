9bb60e036ac0fa663b029a55dff8bdd98e5df7d5
Jayesh Bapu Ahire <jayesh.ahire@traceable.ai>
2020-11-30 09:06:31 +0530

    updates release workflow and gradle plugin (#12)
    * updates release workflow and gradle plugin
    
    * addressed Aaron's comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.1.2"
   id("org.hypertrace.ci-utils-plugin") version "0.1.1"
-  id("org.hypertrace.docker-plugin") version "0.3.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.3.0"
+  id("org.hypertrace.docker-plugin") version "0.8.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.8.0"
 }
 
 group = "org.hypertrace.collector"
@@ -13,3 +13,6 @@ hypertraceDocker {
     dockerFile.set(file("cmd/occollector/Dockerfile"))
   }
 }
+
+
+
